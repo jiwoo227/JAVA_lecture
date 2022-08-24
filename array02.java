ddb9eac1ac598ba84f7bbcca67802639b8037798
최지우 <w2040@e-mirim.hs.kr>
2022-08-25 02:17:39 +0900

    배열

---
+++ array02.java
@@ -0,0 +1,11 @@
+public class array02 {
+    public static void main(String[] args){
+        // 배열 선언과 초기화를 동시에
+        int[] arr2 = {10, 20, 30, 40, 50};
+        System.out.println("arr2[0] : " + arr2[0]);
+        System.out.println("arr2[1] : " + arr2[1]);
+        System.out.println("arr2[2] : " + arr2[2]);
+        System.out.println("arr2[3] : " + arr2[3]);
+        System.out.println("arr2[4] : " + arr2[4]);
+    }
+}
