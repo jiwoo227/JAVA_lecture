527e98d97563c5a6b7c02c95f0b6b7d446a16ce6
최지우 <w2040@e-mirim.hs.kr>
2022-08-25 02:04:12 +0900

    연산자

---
+++ Operator03.java
@@ -0,0 +1,14 @@
+public class Operator03 {
+    public static void main(String[] args){
+
+        //비트연산자
+        int x =2;
+        int y = 3;
+
+        System.out.println("x & y : " + (x & y));
+        System.out.println("x | y : " + (x | y));
+        System.out.println("x ^ y : " + (x ^ y));
+    }
+
+}
+
