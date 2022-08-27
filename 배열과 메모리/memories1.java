663df69cdb128899de483f42106c0c959f69907b
최지우 <w2040@e-mirim.hs.kr>
2022-08-27 23:03:18 +0900

    배열과 메모리

---
+++ 배열과 메모리/memories1.java
@@ -0,0 +1,11 @@
+        // 다차원 배열
+        int[][] arrMul = new int[3][2];
+        arrMul[0][0] = 10;
+        arrMul[0][1] = 100;
+        arrMul[1][0] = 20;
+        arrMul[1][1] = 200;
+        arrMul[2][0] = 30;
+        arrMul[2][1] = 300;
+        System.out.println("arrMul[0] : " + Arrays.toString(arrMul[0]));
+        System.out.println("arrMul[1] : " + Arrays.toString(arrMul[1]));
+        System.out.println("arrMul[2] : " + Arrays.toString(arrMul[2]));
